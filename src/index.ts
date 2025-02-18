2b1fad2ceec2dc81813cc8bfba235d0ee7ec8a54
HuiSou <hui.supat@gmail.com>
2025-02-19 00:02:55 +0700

    backup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+type PlayerAction = { pick:string } 
+const app = new Elysia().post("/api/matches/actions", ({body}) => {
+ const playerAction = body as PlayerAction
+ return { player:playerAction.pick, npc:"rock", result:"inconclusive" }
+}
+).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
